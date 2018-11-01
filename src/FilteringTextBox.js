c6008f3b1b7cb924891b8e8e207b0d76168c6721
AMutaa <piusandru@gmail.com>
2018-11-01 17:20:14 -0400

    fruit_list and testing events on keydown

--- src/FilteringTextBox.js
+++ src/FilteringTextBox.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import search from './search.svg'
 
+
 class FilteringTextBox extends Component {
+  state = {
+    fruits: ['Apple', 'Orange', 'Banana', 'Pineapple', 'Blueberry', 'Blackberry',
+      'Raspberry', 'Cranberry', 'Clementine', 'Mango', 'Papaya', 'Peach', 'Tangerine', 'Pear', 'Plum', 'Grapes',
+      'Boysenberry', 'Lychee', 'Pomegranate', 'Watermelon', 'Honey Dew Melon', 'Fig', 'Cherry', 'Grapefruit']
+  }
   render() {
     return (
       <div>
@@ -9,10 +15,15 @@ class FilteringTextBox extends Component {
         <form action="">
           <div className='input_box'>
             <label htmlFor='fruit'></label>
-            <input type='text' id='fruit' placeholder='Search...' />
+            <input type='text' id='fruit' onKeyDown={handleClick} placeholder='Search...' />
           </div>
           <button type='submit'><img src={search} alt="" id='search' /></button>
         </form>
+        <ul>
+          {this.state.fruits.map((fruit) => (
+            <li>{fruit}</li>
+          ))}
+        </ul>
 
       </div >
     )
@@ -20,3 +31,7 @@ class FilteringTextBox extends Component {
 }
 
 export default FilteringTextBox;
+
+function handleClick(e) {
+  console.log(e.target.value)
+}
