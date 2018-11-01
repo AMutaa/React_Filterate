4f929f4ba7483397cf9d873a62d6e7cc2caf3152
AMutaa <piusandru@gmail.com>
2018-11-01 16:47:21 -0400

    added more styles:

--- src/FilteringTextBox.js
+++ src/FilteringTextBox.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import search from './search.svg'
 
 class FilteringTextBox extends Component {
   render() {
     return (
       <div>
         <h1>Filtering Text Box</h1>
-        <div className='input_box'>
-          <label htmlFor='fruit'></label>
-          <input type='text' id='fruit' />
-        </div>
-      </div>
+        <form action="">
+          <div className='input_box'>
+            <label htmlFor='fruit'></label>
+            <input type='text' id='fruit' placeholder='Search...' />
+          </div>
+          <button type='submit'><img src={search} alt="" id='search' /></button>
+        </form>
+
+      </div >
     )
   }
 }
