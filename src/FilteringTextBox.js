284a6e2d504029ab717d91dca1e2a0dacef98e4b
AMutaa <piusandru@gmail.com>
2018-11-02 17:27:27 -0400

    update

--- src/FilteringTextBox.js
+++ src/FilteringTextBox.js
@@ -3,36 +3,50 @@ import search from './search.svg'
 
 
 class FilteringTextBox extends Component {
-  state = { fruits: [] };
+  state = {
+    fruitSuggetst: [],
+    suggestionVisibility: false,
+    value: ''
+  };
 
 
   changeList = (e) => {
     const fruits = ['Apple', 'Orange', 'Banana', 'Pineapple', 'Blueberry', 'Blackberry',
       'Raspberry', 'Cranberry', 'Clementine', 'Mango', 'Papaya', 'Peach', 'Tangerine', 'Pear', 'Plum', 'Grapes',
       'Boysenberry', 'Lychee', 'Pomegranate', 'Watermelon', 'Honey Dew Melon', 'Fig', 'Cherry', 'Grapefruit']
+
     const updatedFruits = fruits.filter((fruit) => fruit.toLowerCase().includes(e.target.value.toLowerCase()));
     this.setState({
-      fruits: updatedFruits
+      fruits: updatedFruits,
+      suggestionVisibility: true
     })
   }
 
-  render() {
+  changeValue = (e) => {
+    console.log(e.target.textContent)
+  }
 
+
+  render() {
+    const { suggestionVisibility, fruits, value } = this.state
     return (
       <div>
         <h1>Filtering Text Box</h1>
-        <form action="">
+        <form action="" autoComplete='off'>
           <div className='input_box'>
             <label htmlFor='fruit'></label>
-            <input type='text' id='fruit' onKeyDown={this.changeList} placeholder='Search...' />
+            <input type='text' id='fruit' onChange onKeyDown={this.changeList} placeholder='Search...' />
           </div>
           <button type='submit'><img src={search} alt="" id='search' /></button>
         </form>
-        <ul>
-          {this.state.fruits.map((fruit, idx) => (
-            <li key={idx}>{fruit}</li>
-          ))}
-        </ul>
+        <div>
+          {suggestionVisibility && <ul>
+            {fruits.map((fruit, idx) => (
+              <li onClick={this.changeValue} key={idx}>{fruit}</li>
+            ))}
+          </ul>}
+        </div>
+
       </div >
     )
   }
